35bbbe89816cb9fa5c7c4b265baa4c0e2b0f753e
Neta Fruchter <netafruchter@Netas-MacBook-Pro.local>
2024-12-09 13:11:21 +0200

    fifth try

--- Scrabble.java
+++ Scrabble.java
@@ -113,7 +113,7 @@ public class Scrabble {
 				}
 				else{
 					score += wordScore(input);
-					System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points.\n");
+					System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points\n");
 					hand = MyString.remove(hand, input);
 				}
 			}
